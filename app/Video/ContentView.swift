da45f5740b8c17d94744c116733bedaf15247b2f
Aperence <anthony.doeraene@hotmail.com>
2024-09-23 18:02:34 +0200

    change to only connect once to the server

--- app/Video/ContentView.swift
+++ app/Video/ContentView.swift
@@ -5,14 +5,26 @@ import MobileVLCKit
 // https://stackoverflow.com/questions/59806850/vlckit-with-swiftui
 
 struct ContentView: View {
+    // launch the local stream proxy
+    let server = stream_server_new(cString("192.168.0.18"), 8080)
+    
     var body: some View {
         NavigationView{
             VStack{
                 Text("Stream")
                 PlayerView(url: "tcp://127.0.0.1:9000")
                 PlayerView(url: "udp://@127.0.0.1:9001")
+            }.task {
+                DispatchQueue.global(qos: .background).async {
+                    // run the stream server in background
+                    stream_server_run(server)
+                }
             }
         }
+    }
+    
+    static func cString(_ string: String) -> UnsafeMutablePointer<Int8>{
+        UnsafeMutablePointer<Int8>(mutating: (string as NSString).utf8String!)
     }
 }
 
@@ -25,12 +37,6 @@ struct PlayerView: UIViewRepresentable {
     }
 
     func makeUIView(context: Context) -> UIView {
-        // launch the local stream proxy
-        let server = stream_server_new(cString("192.168.0.18"), 8080)
-        DispatchQueue.global(qos: .background).async {
-            // run the stream server in background
-            stream_server_run(server)
-        }
         let controller = UIView()
         mediaPlayer.drawable = controller
         let uri = URL(string: url)
@@ -40,10 +46,6 @@ struct PlayerView: UIViewRepresentable {
         mediaPlayer.libraryInstance.debugLogging = true
         mediaPlayer.libraryInstance.debugLoggingLevel = 3
         return controller
-    }
-
-    func cString(_ string: String) -> UnsafeMutablePointer<Int8>{
-        UnsafeMutablePointer<Int8>(mutating: (string as NSString).utf8String!)
     }
 
     func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
